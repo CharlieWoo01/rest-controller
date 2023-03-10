822188035632cba2bb51505f420bbda45874164c
Charlie Roberts <charliewoo1412@gmail.com>
2023-03-10 16:36:23 +0000

    Add CORS filter & remove mock data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -17,30 +16,18 @@ public class DemoApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository repository) {
-        return args -> {
-            String email = "charliewoo1412@gmail.com";
-            Address address = new Address(
-                    "England",
-                    "London",
-                    "ME58RT"
-            );
-            Student student = new Student(
-                    "Charlie",
-                    "Roberts",
-                    email,
-                    Gender.MALE,
-                    address,
-                    List.of("Computer Science", "Maths"),
-                    BigDecimal.TEN,
-                    LocalDateTime.now()
-            );
-
-            repository.findStudentByEmail(email)
-                    .ifPresentOrElse(s -> System.out.println(s + " already exists"), () -> {
-                        repository.insert(student);
-                        System.out.println("Inserting student " + student);
-                    });
-        };
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }
